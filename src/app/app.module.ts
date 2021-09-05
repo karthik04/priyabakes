fa8fdd0b438c894432533e944d5c059da136f2f7
Karthik <karthik253bc@gmail.com>
2021-09-05 16:27:40 +0530

    Logo change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
 import {MatBadgeModule} from '@angular/material/badge';
 import {MatExpansionModule} from '@angular/material/expansion';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   imports: [
@@ -29,7 +30,8 @@ import {MatExpansionModule} from '@angular/material/expansion';
     MatIconModule,
     MatSnackBarModule,
     MatBadgeModule,
-    MatExpansionModule
+    MatExpansionModule,
+    MatCardModule
   ],
   declarations: [
     AppComponent,
